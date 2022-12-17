56efcd6b477f18eebce9199e71d1cde8a8314993
NandaAmanta <nansamgr@gmail.com>
2022-12-17 13:10:31 +0800

    add anotation service

--- src/main/java/com/exp/ewallet/presist/useCases/UserUseCase.java
+++ src/main/java/com/exp/ewallet/presist/useCases/UserUseCase.java
@@ -11,11 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author ASUS ROG
  */
+@Service
 public class UserUseCase implements UserDetailsService {
 
     /**
